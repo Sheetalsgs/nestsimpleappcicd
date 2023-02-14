73e76d85466cb9c882c00454ce0be84263b96da1
sheetal <sheetalshevalkar265@gmail.com>
2023-02-14 18:07:09 +0530

    second changes to code

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Successfully deploy to azure container web app using gitaction cicd');
     });
   });
 });
