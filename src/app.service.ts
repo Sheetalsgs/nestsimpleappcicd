aa0f1910da5565b990ef3b899e92d8b4b7e7a068
sheetal <sheetalshevalkar265@gmail.com>
2023-02-14 18:40:08 +0530

    final test'

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'azure app deploy through cicd';
+    return 'azure app deploy through cicd...........';
   }
 }
